96bb62aef58fccb2953e02abdb4a9561ca4b6362
Benjamin Eskildsen <benjamin.eskildsen@yale.edu>
2016-06-23 21:11:38 -0400

    eureca example

---
+++ server.js
@@ -0,0 +1,22 @@
+﻿var express = require('express')
+, app = express(app)
+    , server = require('http').createServer(app);
+    var Eureca = require('eureca.io');
+
+    var eurecaServer = new Eureca.Server();
+
+    eurecaServer.attach(server);
+
+
+    //functions under "exports" namespace will be exposed to client side
+    eurecaServer.exports.hello = function () {
+        console.log('Hello from client');
+    }
+//------------------------------------------
+
+//see browser client side code for index.html content
+app.get('/', function (req, res, next) {
+    res.sendfile('index.html');
+});
+
+server.listen(8000);

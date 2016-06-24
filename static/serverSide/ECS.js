bb00438ef43021c42338e5e23c07375f0bd06a4a
Stephen Parsons <stephen.r.parsons@gmail.com>
2016-06-24 10:10:34 -0400

    add reference to ECS inspiration

--- static/serverSide/ECS.js
+++ static/serverSide/ECS.js
@@ -1,3 +1,6 @@
+// The ECS model in this game is inspired by
+// http://vasir.net/blog/game-development/how-to-build-entity-component-system-in-javascript
+
 module.exports = {
     Entity: require('./Entity.js').entity,
     Components: require('./Components.js').components,

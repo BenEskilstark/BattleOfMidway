0e2678d0f74f91432ab554147f04324aa2bb78ad
Stephen Parsons <stephen.r.parsons@gmail.com>
2016-06-23 23:40:52 -0400

    initial ECS working, will need refactoring

--- server.js
+++ server.js
@@ -25,12 +25,43 @@ server.listen(8000);
 
 // eurecaServer.clientProxy.render({});
 
-// ECS fun
+// Demo ECS things. The below could all be abstracted away into a game
+// function in another file, if we wish to make the server file
+// simpler.
 var ECS = require('./static/serverSide/ECS');
+var Assemblages = require('./static/serverSide/Assemblages').assemblages;
 
-// Demo component
 var entity = new ECS.Entity();
 entity.addComponent( new ECS.Components.Health() );
 entity.print();
 entity.components.health.value = 40;
 entity.print();
+
+var entities = {};
+var entity;
+
+for(var i = 0; i < 20; i++) {
+    entity = new ECS.Entity();
+    entity.addComponent( new ECS.Components.Health() );
+    entities[entity.id = entity];
+}
+
+ECS.entities = entities;
+
+entity = new Assemblages.Ship();
+
+entity.print()
+
+// All the systems, in order of execution.
+var systems = [
+    ECS.Systems.health,
+];
+
+function gameLoop() {
+    for(var i = 0, len = systems.length; i < len; i++) {
+	systems[i](ECS.entities);
+    }
+}
+
+// We might want to do this differently.
+// requestAnimationFrame(gameLoop);
